76b9952756f60ccfc84864532513195723036c52
zhan <xmyhx126@126.com>
2018-04-25 22:59:42 +0800

    write a funtion mult

--- test.c
+++ test.c
@@ -3,7 +3,9 @@
 int main()
 {
 	int a = 0, b = 2, c = 3;
-	a = sum(a,c);
+	printf("2 + 3 = %d\n",sum(b, c));
+	printf("2 - 3 = %d\n",sub(b, c));
+	printf("2 * 3 = %d\n",mult(b, c));
 	return 0;
 }
 
@@ -15,4 +17,9 @@ int sum(int sum1, int sum2)
 int sub(int sub1, int sub2)
 {
 	return (sub1 - sub2);
+}
+
+int mult(int mult1, int mult2)
+{
+	return (mult1 * mult2);
 }
--- test.h
+++ test.h
@@ -2,3 +2,4 @@
 
 int sum(int sum1, int sum2);
 int sub(int sub1, int sub2);
+int mult(int mult1, int mult2);
